0ded250ac85b747697c2d9e085ed3e51d2a1b29e
Artem <artem.andreyev212@gmail.com>
2025-02-27 18:02:47 +0200

    Added some components for creating a job

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -12,18 +12,15 @@
                 <p class="mt-1 text-sm/6 text-gray-600">We just need a handful of details from you.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
-                        <div class="mt-2">
-                            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input type="text" name="title" id="title" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="Shift Leader" required>
-                            </div>
+                    <x-form-field>
+                        <x-form-label for="title">Title</x-form-label>
 
-                            @error('title')
-                                <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
-                            @enderror
+                        <div class="mt-2">
+                            <x-form-input name="title" id="title" placeholder="CEO" required/>
+
+                            <x-form-error name="title" />
                         </div>
-                    </div>
+                    </x-form-field>
 
                     <div class="sm:col-span-4">
                         <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
