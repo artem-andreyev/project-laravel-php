1ce7af7af89a021496afcad1e4f7f7b7e7a771c9
Artem <artem.andreyev212@gmail.com>
2025-03-11 21:11:34 +0200

    Started working with mail system

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,14 @@ use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 
+Route::get('test', function() {
+    \Illuminate\Support\Facades\Mail::to('jeffrey@example.com')->send(
+        new \App\Mail\JobPosted()
+    );
+
+    return 'Done';
+});
+
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
