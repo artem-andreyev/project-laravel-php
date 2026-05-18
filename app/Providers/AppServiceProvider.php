fe13c193e009912e6ee08e5263e5b9450c4fa88f
Artem <artem.andreyev212@gmail.com>
2026-05-18 22:44:17 +0300

    added https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,25 +4,19 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         Model::preventLazyLoading();
 
-        // Paginator::useBootstrapFive();
+        URL::forceScheme('https');
     }
 }
