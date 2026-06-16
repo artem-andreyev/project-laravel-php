5ac63d29b9f1a93fceab3bf8daa6cffb4c30a67d
22DP2AAndr <aa00017@rvt.lv>
2026-06-16 20:12:44 +0300

    Add admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,11 +4,23 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        User::updateOrCreate(
+            ['email' => 'admin@project-laravel-php.com'],
+            [
+                'first_name' => 'Project',
+                'last_name' => 'Admin',
+                'password' => Hash::make('Admin2026!panel'),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ]
+        );
+
         User::firstOrCreate(
             ['email' => 'test@example.com'],
             [
